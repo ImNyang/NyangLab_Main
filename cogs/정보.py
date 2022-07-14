3b2d38bb2e65dc10f66ef2b86fd64ab43e2a2697
ImNyang <72956792+ImNyang@users.noreply.github.com>
2022-07-15 07:34:34 +0900

    Update 정보.py
    나 머함

--- cogs/정보.py
+++ cogs/정보.py
@@ -31,6 +31,52 @@ class 정보(commands.Cog):
         embed.add_field(name="☀ㅣ날씨", value=f"{weather}", inline=True)
         embed.add_field(name="🌡️ㅣ온도", value=f"{temperature}°C", inline=True)
         embed.add_field(name="🌡️ㅣ습도", value=f"{humidity}", inline=True)
+    
+    @commands.command(aliases=['서버','서버정보','Guild'])
+    async def GuildInfo(self, ctx):
+        '''
+        Guild Info를 출력합니다.
+        '''
+        g = ctx.guild
+        name = g.name
+        afk_voice = g.afk_channel
+        banner = g.banner
+        system_channel = g.system_channel
+        bitrate_limit = g.bitrate_limit
+        bitrate_limit = bitrate_limit = str(bitrate_limit)
+        bitrate_limit = bitrate_limit.strip("0")
+        bitrate_limit = bitrate_limit.strip("000")
+        bitrate_limit = bitrate_limit.strip(".")
+        emoji_limit = g.emoji_limit 
+        filesize_limit = g.filesize_limit
+        boost_role = g.premium_subscriber_role
+        owner = g.owner
+        icon_url = g.icon_url
+        is_icon_animated = g.is_icon_animated()
+        invite_url_background = g.splash
+        member_count = g.member_count
+        created_at = g.created_at
+        region = g.region
+        #invite = await g.invites()
 
+        embed = discord.Embed(title=f"{name}", description=f"{member_count}명의 맴버들과 함께하고 있어요! 이 멋진 봇과 함께요!", color=0xd6ffdb)
+        embed.set_thumbnail(url=f"{icon_url}")
+        embed.add_field(name="----⚙️일반⚙️----", value="ㅣ모든 서버들이 사용이 가능해요!ㅣ", inline=False)
+        embed.add_field(name="🤿ㅣAFK 음성 채널", value=f"{afk_voice}", inline=True)
+        embed.add_field(name="🗓ㅣ서버 생성일 UTC", value=f"{created_at}", inline=True)
+        embed.add_field(name="🌏ㅣ서버 메인 언어", value=f"{region} (현재 제대로 표시되지 않습니다.)", inline=True)
+        embed.add_field(name="📥ㅣ디스코드 관리자 공지 채널", value=f"{system_channel}", inline=True)
+        embed.add_field(name="----💎부스트💎----", value="ㅣ부스트일 경우 더 많은 것들이 True로 되어 있어요!ㅣ", inline=False)
+        embed.add_field(name="🏷ㅣ부스트 역할", value=f"{boost_role}", inline=True)
+        embed.add_field(name="🌌ㅣ서버 아이콘 움직임", value=f"{is_icon_animated}", inline=True)
+        embed.add_field(name="🖥ㅣ디스코드 초대 링크 배경화면", value=f"{invite_url_background}", inline=True)
+        embed.add_field(name="📃ㅣ서버 배너", value=f"{banner}", inline=True)
+        embed.add_field(name="🎤ㅣ서버 비트레이트 한계", value=f"{bitrate_limit}kbps", inline=True)
+        embed.add_field(name="😀ㅣ이모지 최대 갯수", value=f"{emoji_limit}개", inline=True)
+        embed.add_field(name="🗄ㅣ파일 최대 용량", value=f"{filesize_limit} KIB", inline=True)
+        embed.set_footer(text=f"{name}", icon_url=f"{icon_url}")
+
+        await ctx.send(embed=embed)
+    
 def setup(bot):
     bot.add_cog(정보(bot))
